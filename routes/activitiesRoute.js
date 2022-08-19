10a70f667568cb52dc871529399919bbced5b555
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-08-19 13:16:01 +0700

    fix error

--- routes/activitiesRoute.js
+++ routes/activitiesRoute.js
@@ -17,7 +17,7 @@ activitiesRouter.param("activityId", async (req,res,next,id) => {
 
 activitiesRouter.get("/", activitiesController.get_allAct);
 activitiesRouter.get("/:activityId", activitiesController.get_soloAct);
-activitiesRouter.post("create", activitiesController.create_Act);
+activitiesRouter.post("/create", activitiesController.create_Act);
 activitiesRouter.put("/:activityId", activitiesController.edit_Act);
 activitiesRouter.delete("/:activityId", activitiesController.delete_Act);
 
