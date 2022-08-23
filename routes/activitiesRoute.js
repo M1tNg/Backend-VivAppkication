ba102d92f6bf848543d9d88f8ea9054b5716393d
Thanawit Narumonnantakhan <mingthanawit08@gmail.com>
2022-08-23 16:16:25 +0700

    fix error

--- routes/activitiesRoute.js
+++ routes/activitiesRoute.js
@@ -8,7 +8,7 @@ activitiesRouter.get("/:activityId", activitiesController.get_soloAct);
 activitiesRouter.post("/create", activitiesController.create_Act);
 activitiesRouter.put("/:activityId", activitiesController.edit_Act);
 activitiesRouter.delete("/:activityId", activitiesController.delete_Act);
-activitiesRouter.post("/summaryMonth", activitiesController.sumMonth);
-activitiesRouter.post("/summaryWeek", activitiesController.sumWeek);
+activitiesRouter.get("/summaryMonth", activitiesController.sumMonth);
+activitiesRouter.get("/summaryWeek", activitiesController.sumWeek);
 
 module.exports = activitiesRouter;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,18 +1,18 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const activitiesRouter = require('./activitiesRoute');
-const scheduleRouter = require('./scheduleRoute');
-const {protect} = require("../middleware/jwtMiddleware");
-const {  registerUser,
-    loginUser,
-    getMe } = require("../controller/userController");
+const activitiesRouter = require("./activitiesRoute");
+const scheduleRouter = require("./scheduleRoute");
+const { protect } = require("../middleware/jwtMiddleware");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controller/userController");
 
-
-router.post('/', registerUser);
-router.post('/login',loginUser);
-router.get('/me', getMe);
-router.use('/me/activities',protect,activitiesRouter );
-router.use('/me/schedule',protect,scheduleRouter );
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", getMe);
+router.use("/me/activities", protect, activitiesRouter);
+router.use("/me/schedule", protect, scheduleRouter);
 
 module.exports = router;
-
